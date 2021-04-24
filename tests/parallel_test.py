b6c4c5a4b0f74b40a025735bc1896f65e222e337
Jiří Fišer <jiri.fiser@ujep.cz>
2021-04-24 13:32:41 +0200

    + test for parallel constructs

---
+++ tests/parallel_test.py
@@ -0,0 +1,21 @@
+#from concurrent.futures import ProcessPoolExecutor as Executor
+from mpi4py.futures import MPIPoolExecutor as Executor
+
+from sigpipes.sigfilter import Hilbert
+
+from sigpipes.psigoperator import AsFuture, Barrier, ProcessTracker
+from sigpipes.sigoperator import Identity, RangeNormalization, Tee, Csv, Print, Fork, MVNormalization
+
+from sigpipes.sources import SigGenerator, SignalType
+
+
+if __name__ == "__main__":
+    with Executor(4) as executor:
+        if executor is not None:
+            source = SigGenerator(100, SignalType.SINUS, 2, 5).sigcontainer()
+            #future =  source | ProcessTracker() | AsFuture(executor) | Tee(RangeNormalization() | Csv(dir="/tmp") | ProcessTracker()) | Barrier() | ProcessTracker()
+
+            futures = (source | ProcessTracker() | AsFuture(executor)
+                              | Fork(RangeNormalization() | ProcessTracker(), MVNormalization() | ProcessTracker())
+                              | ProcessTracker() | Barrier() | ProcessTracker())
+            print(futures)
